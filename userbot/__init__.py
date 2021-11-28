ee8f5589586252789a202400e5e954e8033f84c5
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-11-28 08:16:39 +0530

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -9,7 +9,7 @@ from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
 __version__ = "3.0.5"
 __license__ = "GNU Affero General Public License v3.0"
-__author__ = "CatUserBot <https://github.com/Jisan09/catuserbot>"
+__author__ = "CatUserBot <https://github.com/TeamFussion/Fussion>"
 __copyright__ = "CatUserBot Copyright (C) 2020 - 2021  " + __author__
 
 catub.version = __version__
@@ -21,11 +21,12 @@ StartTime = time.time()
 catversion = "3.0.4"
 
 if Config.UPSTREAM_REPO == "badcat":
-    UPSTREAM_REPO_URL = "https://github.com/Jisan09/catuserbot"
+    UPSTREAM_REPO_URL = "https://github.com/TeamFussion/Fussion"
 elif Config.UPSTREAM_REPO == "goodcat":
-    UPSTREAM_REPO_URL = "https://github.com/sandy1709/catuserbot"
+    UPSTREAM_REPO_URL = "https://github.com/TeamFussion/Fussion"
 else:
-    UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
+   # UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
+     UPSTREAM_REPO_URL = "https://github.com/TeamFussion/Fussion"
 
 if Config.PRIVATE_GROUP_BOT_API_ID == 0:
     if gvarstatus("PRIVATE_GROUP_BOT_API_ID") is None:
