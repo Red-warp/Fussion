0fcf638a8daa75cacfb87b9aa9772de1362dc612
Red warp <88662082+TeamFussion@users.noreply.github.com>
2021-11-28 08:52:40 +0530

    Update startup.py

--- userbot/utils/startup.py
+++ userbot/utils/startup.py
@@ -63,9 +63,9 @@ async def startupmessage():
         if BOTLOG:
             Config.CATUBLOGO = await catub.tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/4e3ba8e8f7e535d5a2abe.jpg",
-                caption="**Your CatUserbot has been started successfully.**",
-                buttons=[(Button.url("Support", "https://t.me/catuserbot"),)],
+                "https://te.legra.ph/file/1b87bff0d89a0ba1f71f4.jpg",
+                caption="**Your Dark Fusion has been started successfully.**",
+                buttons=[(Button.url("Support", "https://t.me/Darkfussion"),)],
             )
     except Exception as e:
         LOGS.error(e)
