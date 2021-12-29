155b50eb47d689cfd8fbed5adb0786754e645eac
TeamFussion <88662082+Red-warp@users.noreply.github.com>
2021-12-29 13:40:40 +0530

    Update inlinebot.py

--- userbot/core/inlinebot.py
+++ userbot/core/inlinebot.py
@@ -55,26 +55,16 @@ def ibuild_keyboard(buttons):
 def main_menu():
     text = f"**Bᴏᴛ Oғ {user}\n\n Mᴀɪɴ Mᴇɴᴜ\n Pʟᴜɢɪɴs ~ 279\n\n Ⲃⲟⲧ Ⲓⲛϝⲟʀⲙⲁⲧⲓⲟⲛ\n ╭━━━━━━━━━━━━━━━➣\n ┣⪼ Ⲟⲱⲛⲉʀ - {user}\n ┣⪼ Ⲃⲟⲧ Ⳳⲉʀⲋⲓⲟⲛ - 1.2.7\n ┣⪼ Ⳙⲣⲧⲓⲙⲉ - 2m.42s\n ┣⪼ Ⲧⲉⳑⲉⲧⲏⲟⲛ - 1.23.0\n ╰━━━━━━━━━━━━━━━➣**"
     buttons = [
-        (Button.inline("ℹ️ Info", data="check"),),
+        (Button.inline("🔌 Pʟᴜɢɪɴs 🔌", data="check"),),
+        
+            Button.inline(f"📍 Pɪɴɢ 📍", data="menu"),
+            (Button.inline(f"🧑‍💻 Uᴘᴅᴀᴛᴇs 🧑‍💻", data="bot_menu"),
+        
         (
-            Button.inline(f"👮‍♂️ Admin ({len(GRP_INFO['admin'])})", data="menu"),
-            Button.inline(f"🤖 Bot ({len(GRP_INFO['bot'])})", data="bot_menu"),
+            Button.inline(f"💖 Sᴜᴘᴘᴏʀᴛ 💖", data="fun_menu"),
+           ( Button.inline(f"⚙ Sᴇᴛᴛɪɴɢs ⚙", data="misc_menu"),
         ),
-        (
-            Button.inline(f"🎨 Fun ({len(GRP_INFO['fun'])})", data="fun_menu"),
-            Button.inline(f"🧩 Misc ({len(GRP_INFO['misc'])})", data="misc_menu"),
-        ),
-        (
-            Button.inline(f"🧰 Tools ({len(GRP_INFO['tools'])})", data="tools_menu"),
-            Button.inline(f"🗂 Utils ({len(GRP_INFO['utils'])})", data="utils_menu"),
-        ),
-        (
-            Button.inline(f"➕ Extra ({len(GRP_INFO['extra'])})", data="extra_menu"),
-            Button.inline(
-                f"⚰️ Useless ({len(GRP_INFO['useless'])})", data="useless_menu"
-            ),
-        ),
-        (Button.inline("🔒 Close Menu", data="close"),),
+        (Button.inline("🚫 Cʟᴏsᴇ 🚫", data="close"),),
     ]
 
     return text, buttons
